44483ff89d9adc0e1dcaa9f150daa949f6652fce
gedebrawidya <gedepujaa9@gmail.com>
2026-06-13 04:48:25 +0700

    setup

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(100)->hasPost(100)->create();
+        $this->call(UserSeeder::class);
     }
 }
